6a734938e1cecc4eaf85c56612c4078057ed16f6
Anantajit Subrahmanya <anantajit@davinci.local>
2022-04-26 17:03:42 -0500

    basic shading for single light and sphere

--- prototyping/raytracer.c
+++ prototyping/raytracer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <math.h>
 
 enum { width = 640, height = 480 };
@@ -12,18 +13,21 @@ int main(void) {
     int f = 50;
     int camera[3] = {320, 240, 0};
 	// X, Y, Z, Radius
-    int sphere[4] = {300, 200, f + 100, 100};
+    int sphere[4] = {320, 200, f + 100, 100};
 
-    int light[3] = {320, 275, f}; // right above sphere, closer to camera
-
+    int light[3] = {300, 100, f}; // right above sphere, closer to camera
+    long intensity = 1000000;
+    
 	int x, y;
 
 	p = pixels;
+    
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
 	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-            
-            
-            
+        for (x = 0; x < width; x++) {
 			unsigned char R, G, B; // Set RGB
             
             /*A and C are swapped from standard quadratic!*/
@@ -53,27 +57,28 @@ int main(void) {
             
             int epsilon = 1;
             
-            unsigned char exposed_light;
+            int exposed_light;
             exposed_light = 10;
             
             if(b * b > 4 * a * c){ // there is a collision
                 t = (-b - sqrt(b * b - 4 * a * c)); // t*c value
-                long light_projection = (light[0] - cx) * (light[0] - cx) + (light[1] - cy) * (light[1] - cy) + (light[2] - cz) * (light[2] - cz);
+                long source_distance = (light[0] - cx) * (light[0] - cx) + (light[1] - cy) * (light[1] - cy) + (light[2] - cz) * (light[2] - cz);
                 if(t > epsilon) { // some epsilon value... if the collision is caused NOT by the first bounce point
-                    
-                        exposed_light = 255 - sqrt(light_projection); // no collision should be bright
+                    exposed_light = intensity/source_distance; // no collision should be bright
                 }
             }
             else
-                exposed_light = 25;
+                exposed_light = 10;
             
-            if(exposed_light < 25)
-                exposed_light = 25;
+            if(exposed_light < 10)
+                exposed_light = 10;
+            if(exposed_light > 255)
+                exposed_light = 255;
             
             if(intersect_sphere){
                 R = exposed_light;
                 G = R; // draw the realized circle in white
-                B = R;
+//                B = R;
 //                printf("INTERSECT: (%d, %d) -> %d\n", x, y, b * b - 4 * a * c);
             }
             else {
@@ -83,12 +88,18 @@ int main(void) {
             }
             
             
-            
 			*p++ = B;
 			*p++ = G;
 			*p++ = R;
 		}
 	}
+    
+    end = clock();
+    
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("%0.2f FPS\n", 1/cpu_time_used, CLOCKS_PER_SEC); //in seconds
+    
 	tga[2] = 2;
 	tga[12] = 255 & width;
 	tga[13] = 255 & (width >> 8);
@@ -96,6 +107,6 @@ int main(void) {
 	tga[15] = 255 & (height >> 8);
 	tga[16] = 24;
 	tga[17] = 32;
-//	return 0;
-	return !((1 == fwrite(tga, sizeof(tga), 1, stdout)) &&  (1 == fwrite(pixels, sizeof(pixels), 1, stdout)));
+	return 0;
+//	return !((1 == fwrite(tga, sizeof(tga), 1, stdout)) &&  (1 == fwrite(pixels, sizeof(pixels), 1, stdout)));
 }
