82345843acc2a9ff32c18076e318432ebd78cb73
Anantajit Subrahmanya <anantajit@davinci.local>
2022-04-27 23:11:22 -0500

    multiple light sources and objects in a scene

--- prototyping/raytracer.c
+++ prototyping/raytracer.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 enum { width = 640, height = 480 };
 
-const int INTENSITY_CALIBRATION = 100;
+const int INTENSITY_CALIBRATION = 75;
+const int DEROUNDER = 75;
+const int ATTENUATION_CUTOFF = 25;
+const char ENABLE_OUTPUT = 1;
 
 /*
  * Checks if a ray originating at p0 and passing through p1 intersects with a sphere at any point
@@ -19,8 +23,8 @@ char ray_sphere_intersection(int sphere[4], int p0[3], int p1[3], int px1[3], in
     
     if(b * b >= 4 * a * c) {
         //CLOCK CYCLE 2: dual square root computation
-        long T1 = (-b  + sqrt(b * b - 4 * a * c)) - a/10; // these are 2a times the actual value we need to plug in. A only depends on the focal length though....
-        long T2 = (-b  - sqrt(b * b - 4 * a * c)) - a/10;
+        long T1 = (-b  + ((int) sqrt(b * b - 4 * a * c))) - a/DEROUNDER; // these are 2a times the actual value we need to plug in. A only depends on the focal length though....
+        long T2 = (-b  - ((int) sqrt(b * b - 4 * a * c))) - a/DEROUNDER;
         
         // CLOCK CYCLE 3
         if(T1 < THRESHOLD && T2 < THRESHOLD)
@@ -44,51 +48,109 @@ char ray_sphere_intersection(int sphere[4], int p0[3], int p1[3], int px1[3], in
 }
 
 int raytracer(void) {
-	char ENABLE_OUTPUT = 1;
 	static unsigned char pixels[width * height * 3];
 	static unsigned char tga[18];
 	unsigned char *p;
 
     /* DO NOT SET f > 10k at the risk of overflowing*/
+   
+    /*
+     SCENE INFORMATION
+     */
+    
+    
 	int f = 1000; // smaller FOV means you can see more, but there will be more distortion
 	int camera[3] = {320, 240, 0};
 	// X, Y, Z, Radius
-	int sphere[4] = {0, 240, f + 201, 100};
-
+    const char SPHERE_COUNT = 2;
+    int sphere_array[SPHERE_COUNT][4] = {{320, 480, f + 200, 200}, {260, 150, f + 200, 50}};
 	// X, Y, Z, Intensity... for a point source
-	int light[4] = {220, 0, f + 201, 200000}; // light from the camera
-
+    const char LIGHT_COUNT = 2;
+    int light_array[LIGHT_COUNT][4] = {{320, -100, f + 200, 100000}, {0, 350, f + 100, 50000}}; // light from the camera
+    
+    
+    /* END OF SCENE INFORMATION...*/
+    
+    
 	int x, y;
 
 	const int resolution = 1; // used for debugging
 
-	p = pixels;
-	for (y = 0; y < height; y+=resolution) {
+    p = pixels;
+    clock_t t;
+    t = clock();
+    for (y = 0; y < height; y+=resolution) {
 		for (x = 0; x < width; x+=resolution) {
-			unsigned char R, G, B; // Set RGB
+			unsigned char R = 0, G = 0, B = 0; // Set RGB
 
 			int ix1[3], strike_pt[3], ix3[3];
 			int screen_pixel[3] = {x, y, f};
 
-			char intersect_sphere = ray_sphere_intersection(sphere, camera, screen_pixel, ix1, strike_pt, 0, 0); // ix2 is closer than ix1
+            char intersect_sphere = 0;
+            
+            // Does the pixel hit ANY sphere at all?
+            for(int sphere_idx = 0; sphere_idx < SPHERE_COUNT; sphere_idx++){
+                int sphere[4];
+                sphere[0] = sphere_array[sphere_idx][0];
+                sphere[1] = sphere_array[sphere_idx][1];
+                sphere[2] = sphere_array[sphere_idx][2];
+                sphere[3] = sphere_array[sphere_idx][3];
+                
+                intersect_sphere += ray_sphere_intersection(sphere, camera, screen_pixel, ix1, strike_pt, 0, 0); // ix2 is closer than ix1
+            }
+            
 
 			if(intersect_sphere){
                 // check for intersection between the collision point and the light source
-                intersect_sphere = ray_sphere_intersection(sphere, strike_pt, light, ix1, ix3, 1, 5);
-                
-                if(intersect_sphere) {
-                    R = 50;
-                    G = 50;
-                    B = 0;
-                } else {
-                    long distance2 = (light[0] - ix2[0]) * (light[0] - ix2[0]) + (light[1] - ix2[1]) * (light[1] - ix2[1]) + (light[2] - ix2[2]) * (light[2] - ix2[2]); // distance to light source
+                for(int light_idx = 0; light_idx < LIGHT_COUNT; light_idx++){
                     
-                    int intensity = (INTENSITY_CALIBRATION * light[3])/(distance2);
-                    intensity = intensity > 255 ? 255 : intensity;
-                    R = intensity > 50 ? intensity : 50;
-                    G = intensity > 50 ? intensity : 50;
-                    B = 0;
+                    int light[4];
+                    light[0] = light_array[light_idx][0];
+                    light[1] = light_array[light_idx][1];
+                    light[2] = light_array[light_idx][2];
+                    light[3] = light_array[light_idx][3];
+                    
+                    // Does ANY sphere block it?
+                    intersect_sphere = 0;
+                    
+                    for(int sphere_idx = 0; sphere_idx < SPHERE_COUNT; sphere_idx++){
+                        int sphere[4];
+                        sphere[0] = sphere_array[sphere_idx][0];
+                        sphere[1] = sphere_array[sphere_idx][1];
+                        sphere[2] = sphere_array[sphere_idx][2];
+                        sphere[3] = sphere_array[sphere_idx][3];
+                        
+                        intersect_sphere += ray_sphere_intersection(sphere, strike_pt, light, ix1, ix3, 1, 10); // ix2 is closer than ix1
+                    }
+                    
+                    
+                    if(intersect_sphere) {
+                        R += 0;
+                        G += 0;
+                        B += 0;
+                    } else {
+                        long distance2 = (light[0] - strike_pt[0]) * (light[0] - strike_pt[0]) + (light[1] - strike_pt[1]) * (light[1] - strike_pt[1]) + (light[2] - strike_pt[2]) * (light[2] - strike_pt[2]); // distance to light source
+                        
+                        int intensity = (INTENSITY_CALIBRATION * light[3])/(distance2); // how much light to add
+                        
+                        if(intensity + R > 255)
+                            R = 255;
+                        else
+                            R += intensity;
+                        if(intensity + G > 255)
+                            G = 255;
+                        else
+                            G += intensity;
+                        if(intensity + B > 255)
+                            B = 255;
+                        else
+                            B += 0;
+                    }
                 }
+                
+                B = B > ATTENUATION_CUTOFF ? B : ATTENUATION_CUTOFF;
+                G = G > ATTENUATION_CUTOFF ? G : ATTENUATION_CUTOFF;
+                R = R > ATTENUATION_CUTOFF ? R : ATTENUATION_CUTOFF;
 			}
 			else {
 				R = 0;
@@ -101,6 +163,10 @@ int raytracer(void) {
 			*p++ = R;
 		}
 	}
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    
+    
 	tga[2] = 2;
 	tga[12] = 255 & width;
 	tga[13] = 255 & (width >> 8);
@@ -110,8 +176,11 @@ int raytracer(void) {
 	tga[17] = 32;
 	if(ENABLE_OUTPUT)
 		return !((1 == fwrite(tga, sizeof(tga), 1, stdout)) &&  (1 == fwrite(pixels, sizeof(pixels), 1, stdout)));
-	else
-		return 0;
+    else{
+        printf("This scene could render at up to %f FPS!\n", 1/time_taken);
+        return 0;
+    }
+        
 }
 
 
